6fb3d6427c3c8a3bea7ebde1b7b4f5c7295c4ed9
Henripper <henri.potter@gmail.com>
2019-05-08 23:27:27 +0300

    Added "Quit" option to main menu of game
    Quit option lets the player quit the game from the main menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,4 +7,9 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
 }
