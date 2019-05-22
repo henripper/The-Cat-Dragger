5292b9bb79c8569dc4dca8d4d47f12fa7c065315
Henripper <henri.potter@gmail.com>
2019-05-22 20:33:49 +0300

    Installed ProBuilder plugin and created worlds
    - Imported now grass material
    - Installed ProBuilder addon for unity
    - Remade Ramp asset prefab
    - Downloaded Textures and materials and a wooden crate prefab
    - Added content to level 5
    - Added content to level 6

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 public class GameManager : MonoBehaviour
 
 {
+    
     public GameObject pauseMenuUI;
     bool gameIsPaused = false;
     bool gameHasEnded = false;
